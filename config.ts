9ffc909e9ef339186cfbad5152c8a0656be4d67b
Rong-bit <hcj382000@gmail.com>
2025-12-31 17:28:28 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -8,7 +8,6 @@ export const SYSTEM_ACCESS_CODE = '888888';
 // 在此處新增使用者的 Email，部署後這些使用者即可在任何裝置登入，無需輸入密碼
 export const GLOBAL_AUTHORIZED_USERS = [
   // 您可以在此加入其他使用者的 Email
-   'hcj382000@gmail.com',
    'chenyu1679@gmail.com',
    'yuzhen422@gmail.com',
    'kkkkk@gmail.com',
